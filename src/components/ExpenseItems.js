6456f9cce9ca24612e73ee5906b01b355bbe5126
27sandesh <kamblesandesh254@gmail.com>
2023-04-07 16:12:35 +0530

    reactcode1

---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,11 @@
+ function ExpenseItem(){
+    return <div>
+        <div>
+            <h2>food 10rs</h2>
+            <h2>petrol 100rs</h2>
+            <h2>movie 200rs</h2>
+        </div>
+    </div>
+            
+ }
+ export default ExpenseItem;
