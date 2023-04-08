8ed6a369f7f47f2d056a8dab0c0d200c95cd4d4d
27sandesh <kamblesandesh254@gmail.com>
2023-04-08 15:27:41 +0530

    passing data

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
-import logo from './logo.svg';
+
 import './App.css';
 import ExpenseItem from './components/ExpenseItems';
+function app(){
+  const expenses=[
+    {
+    id:'el',
+    title:'Title Paper',
+    amount:23,
+    date:new Date(2022, 7, 14),
+    location:'new York',
+  },
+  {
+    id:'el1',
+    title:'New Tv',
+    amount:799,
+    date:new Date(2022, 8, 14),
+    location:'LA',
+  },
+  {
+    id:'el2',
+    title:'Car Insurance',
+    amount:25,
+    date:new Date(2022, 9, 14),
+    location:'UK',
+  },
+  {
+    id:'el3',
+    title:'food',
+    amount:30,
+    date:new Date(2022, 4, 14),
+    location:'UAE',
+  },
+]
+return (
+  <div>
+    <h2>Lets get started with react</h2>
+    <p>this is also visible</p>
+  {
+    expenses.map((props)=>(
+      <ExpenseItem 
+      key={props.id}
+      title={props.title}
+      amount={props.amount}
+      date={props.date}
+      location={props.location}></ExpenseItem>
+    ))
+  }
+  </div>
+);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <ExpenseItem></ExpenseItem>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React js
-        </a>
-      </header>
-    </div>
-  );
 }
 
-export default App;
+export default app;
