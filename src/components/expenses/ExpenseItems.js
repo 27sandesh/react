2a4a69094b37995d689624d416366de9245055d3
27sandesh <kamblesandesh254@gmail.com>
2023-04-09 13:26:50 +0530

    reeventlinster

--- src/components/expenses/ExpenseItems.js
+++ src/components/expenses/ExpenseItems.js
@@ -3,6 +3,12 @@ import './ExpenseDate'
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
  const ExpenseItem=(props)=>{
+    const deletebtn=()=>{
+        const ExpenseItem=document.getElementsByClassName('expense-item__description')[0];
+        ExpenseItem.remove();
+    }
+  
+
 
     return (
         <div className='expense-item'>
@@ -10,6 +16,7 @@ import ExpenseDetails from './ExpenseDetails';
             <ExpenseDetails title={props.title}/>
             <ExpenseDetails location={props.location}/>
             <ExpenseDetails amount={props.amount}/>
+            <button onClick={deletebtn}>delete</button>
         </div>
     )
             
